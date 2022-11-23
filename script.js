1883c276eb5b568b6a22b32fde93692193e5c739
BuildTools <unconfigured@null.spigotmc.org>
2022-11-23 15:47:52 +0000

    stuff on screen

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-import {pokemonArray} from "./data/pokemon.js";
+import { pokemonArray } from "./data/pokemon.js";
 
 const cardContainer = document.querySelector(".card-container");
 
-console.log(pokemonArray);
+// console.log(pokemonArray);
+
+const renderCard = (pokemonArray) => { 
+    let htmlString = "";
+
+    pokemonArray.forEach((pokemon) => {
+        htmlString += `
+        <section>
+            <img>${pokemon.sprite}</img>    
+            <h1>${pokemon.name}</h1>
+        </section>
+        
+        `;
+    });
+
+    cardContainer.innerHTML = htmlString;
+
+};
+renderCard(pokemonArray);
